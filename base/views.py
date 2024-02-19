384f3ac195a967cac0f86fe8f1ea5ca8cc3b9d28
kibeert <ckibertkoech@gmail.com>
2024-02-19 13:49:08 +0300

    Added homepage and about views to base app.

--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def homepage(request, *args, **kwargs):
+    return HttpResponse("wELCOME TO OUR HOME PAGE")
+
+def about(reauest, *args, **kwargs):
+    return HttpResponse("Welcome to our homepage")
--- nautic/urls.py
+++ nautic/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from base import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),

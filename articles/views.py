978ca00fecec5b37abc4ec488bc20ec24a89b6b2
kibeert <ckibertkoech@gmail.com>
2024-02-19 14:58:40 +0300

    Update article list view and remove article list template

--- articles/views.py
+++ articles/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def articlelist(request, *args, **kwargs):
-    return render(request, "templates/article_list.html", {})
+    return render(request, "articles/article_list.html", {})

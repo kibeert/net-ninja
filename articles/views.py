c31605bd2a5fa5d2235e673ee12d036173fa4209
kibeert <ckibertkoech@gmail.com>
2024-02-19 15:19:08 +0300

    Changes to article list view and urls.

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def articlelist(request, *args, **kwargs):
-    return render(request, "articles/article_list.html", {})
+def atriclelist(request, *args, **kwargs):
+    return render(request, 'artcles/article_list.html')
--- nautic/urls.py
+++ nautic/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from .import views
 
 
 urlpatterns = [
     path('', views.homepage, name='home'),
     path('about/', views.about, name='about'),
-    
+    path('article/', include('articles.urls')),
     path('admin/', admin.site.urls),
 ]

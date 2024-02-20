0d15c881bb3bbe00258def5d1c68d0309f49beb6
kibeert <ckibertkoech@gmail.com>
2024-02-20 09:33:30 +0300

    Update article view and templates

--- articles/views.py
+++ articles/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def article_view(request, *args, **kwargs):
-    return render(request, "templates/article_list.html")
+    return render(request, "articles/articles_list.html")
--- nautic/settings.py
+++ nautic/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'nautic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        'DIRS': [Path.joinpath(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

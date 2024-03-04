118240871beb2c292bb76eb1f9ca67d82549273d
kibeert <ckibertkoech@gmail.com>
2024-03-04 09:14:58 +0300

    Update article detail view and URL patterns.

--- articles/urls.py
+++ articles/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from .import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from .views import ArticleDetailView
 
 urlpatterns = [
     path('article/', views.articleview, name='articles'),
     path('slug/', views.article_detail ,name="article-detail"),
+    path('articles/<slug:slug>/', ArticleDetailView.as_view(), name='article_detail'),
     
 ]
 urlpatterns += staticfiles_urlpatterns()
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Article
+from django.views import View
 
 # Create your views here.
 def articleview(request, *args, **kwargs):
@@ -10,3 +11,19 @@ def articleview(request, *args, **kwargs):
 
 def article_detail(request, *args, **kwargs):
     return HttpResponse("Hello world")
+
+from django.shortcuts import render, get_object_or_404
+from .models import Article  # Make sure to import your Article model
+from django.views import View
+
+class ArticleDetailView(View):
+    template_name = 'independent.html'  # Replace with your actual template name
+
+    def get(self, request, slug):
+        # Retrieve the article with the given slug or ID
+        article = get_object_or_404(Article, slug=slug)
+
+        # You can add additional logic here if needed
+
+        # Render the article detail template with the article context
+        return render(request, self.template_name, {'article': article})
